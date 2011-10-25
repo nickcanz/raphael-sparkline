b4ab377bbeb98567cff03f6d4a302493651bf176
Nicholas Canzoneri <ac3522@gmail.com>
2011-10-24 22:42:40 -0400

    Adding fromJquery method to raphael

--- example.js
+++ example.js
@@ -1,8 +1,14 @@
 var Examples = (function () {
   var self = {};
   self.init = function () {
-    var paper = Raphael('example', 200, 50);
-    paper.sparkline([5,1,2,3,4,5]);
+    var paper = Raphael.fromJquery($('#example'));
+
+    var data = [];
+    for(var i=0;i<100;i++) {
+      data.push(Math.floor(Math.random() * 100));
+    }
+
+    paper.sparkline(data);
   };
   return self;
 }());
